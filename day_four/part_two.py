83605eae7f9faccea7bf7de1ca4462a0dc6c39fd
eggmaninfinity <joshuathomasrutter@gmail.com>
2024-04-09 10:51:06 +0200

    day four ho ho ho now i have a repository

---
+++ day_four/part_two.py
@@ -0,0 +1,41 @@
+'''
+take line
+make section1, seaction2
+if (section1[0] < section2[0] AND section1[2] > section2[2]) OR 
+(section2[0] < section1[0] AND section2[2] > section1[2]):
+    sum += 1
+
+
+lines = [
+    '2-4,6-8',
+    '2-3,4-5',
+    '5-7,7-9',
+    '2-8,3-7',
+    '6-6,4-6',
+    '2-6,4-8'
+]
+'''
+
+with open('input.txt') as file:
+    lines = file.readlines()
+
+    sum = 0
+
+    for line in lines:
+        split_line = line.split(',')
+        section1 = split_line[0]
+        section2a = split_line[1]
+        section2 = section2a[:-1]
+        section1_split = section1.split('-')
+        section2_split = section2.split('-')
+        num1_1 = section1_split[0]
+        num1_2 = section1_split[1]
+        num2_1 = section2_split[0]
+        num2_2 = section2_split[1]
+        range_one = [i for i in range(int(num1_1), int(num1_2) + 1)]
+        range_two = [i for i in range(int(num2_1), int(num2_2) + 1)]
+        for i in range_one:
+            if i in range_two:
+                sum += 1
+                break
+    print(sum)
